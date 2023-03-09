bf4d90b0bd50af579db62e7f64cf0a84f6512905
Meroke <3154911544@qq.com>
2023-03-09 11:01:31 +0800

    Gesture operation of image scaling. (Maloperation)

---
+++ app/src/main/java/com/duoshine/camerax/ResizableImageView.kt
@@ -0,0 +1,49 @@
+package com.duoshine.camerax
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.ImageView
+import java.lang.Math.sqrt
+
+class ResizableImageView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
+
+    private var mLastTouchX: Float = 0f
+    private var mLastTouchY: Float = 0f
+    private var mLastHeight: Int = 0
+    private var mLastWidth: Int = 0
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val action = event.action
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                mLastTouchX = event.x
+                mLastTouchY = event.y
+                mLastHeight = height
+                mLastWidth = width
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val deltaX = event.x - mLastTouchX
+                val deltaY = event.y - mLastTouchY
+                val distance = sqrt(deltaX * deltaX + deltaY * deltaY.toDouble()).toFloat()
+
+                val scale = distance / 217 // Adjust this value as needed
+                var newWidth = (mLastWidth * scale).toInt()
+                var newHeight = (mLastHeight * scale).toInt()
+
+                if( newWidth < 80)
+                    newWidth = 80
+                if( newWidth > 750)
+                    newWidth = 750
+                if (newHeight < 80)
+                    newHeight = 80
+                if( newHeight > 750)
+                    newHeight = 750
+                layoutParams.width = newWidth
+                layoutParams.height = newHeight
+                requestLayout()
+            }
+        }
+        return true
+    }
+}
